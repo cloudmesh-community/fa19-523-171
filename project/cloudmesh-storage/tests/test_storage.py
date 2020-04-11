08966b4cae3f0314ce67ea5d83cf1e4981317c9a
Gregor von Laszewski <laszewski@gmail.com>
2020-04-11 09:07:08 -0400

    outdated code auto update

--- project/cloudmesh-storage/tests/test_storage.py
+++ project/cloudmesh-storage/tests/test_storage.py
@@ -9,7 +9,7 @@ from pprint import pprint
 
 import pytest
 from cloudmesh.common.StopWatch import StopWatch
-from cloudmesh.common3.Benchmark import Benchmark
+from cloudmesh.common.Benchmark import Benchmark
 from cloudmesh.common.parameter import Parameter
 from cloudmesh.common.util import HEADING
 from cloudmesh.common.util import path_expand
